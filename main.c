6e78d40039c45ec19f3defb8817986eee003d5ff
hudaelarnaout <85964817+hudaelarnaout@users.noreply.github.com>
2021-06-15 23:22:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 int main()
 {
-  int i;
-  for (i = 1=0; i < 32; i++)
-  {
-    printf("Hello School\n");
-  }
-  return 0;
+int i;
+for (i = 0; i < 32; i++)
+{
+printf("Hello School\n");
+}
+return 0;
 }
